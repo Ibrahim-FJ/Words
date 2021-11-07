fa637d1135ec2996361985a4e5d779902582945e
Ibrahim Alfaifi <Ibrahim.farhan.ruh.k@tuwaiq.edu.sa>
2021-11-07 13:37:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/wordsapp/MainActivity.kt
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.wordsapp
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.GridLayout
+import android.widget.GridView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.wordsapp.databinding.ActivityMainBinding
+
+/**
+ * Main Activity and entry point for the app. Displays a RecyclerView of letters.
+ */
+class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private var isLinearLayoutManager = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        recyclerView = binding.recyclerView
+        // Sets the LinearLayoutManager of the recyclerview
+        changeLayout()
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.layout_menu, menu)
+        menu?.let { changeIcon(it.findItem(R.id.item1)) }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item1 -> {
+                isLinearLayoutManager = !isLinearLayoutManager
+                changeLayout()
+                changeIcon(item)
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+        return true
+    }
+
+    private fun changeIcon(menuItem: MenuItem) {
+        menuItem.icon = if (isLinearLayoutManager) {
+            ContextCompat.getDrawable(this, R.drawable.ic_baseline_grid_on_24)
+        } else {
+            ContextCompat.getDrawable(this, R.drawable.ic_baseline_menu_24)
+        }
+    }
+
+    private fun changeLayout() {
+
+        if (isLinearLayoutManager) {
+            recyclerView.layoutManager = LinearLayoutManager(this)
+        } else {
+            recyclerView.layoutManager = GridLayoutManager(this, 3)
+        }
+        recyclerView.adapter = LetterAdapter()
+    }
+
+}
